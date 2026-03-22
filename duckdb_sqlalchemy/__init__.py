595aeafb89a77ab0ce3df321b2e41e03087b7024
Leonardo Vida <lleonardovida@gmail.com>
2026-03-22 17:54:11 +0100

    chore: release 1.4.4.6

--- duckdb_sqlalchemy/__init__.py
+++ duckdb_sqlalchemy/__init__.py
@@ -68,7 +68,7 @@ else:
         _pg_base, "PGExecutionContext", DefaultExecutionContext
     )
 
-__version__ = "1.4.4.5"
+__version__ = "1.4.4.6"
 sqlalchemy_version = sqlalchemy.__version__
 SQLALCHEMY_VERSION = Version(sqlalchemy_version)
 SQLALCHEMY_2 = SQLALCHEMY_VERSION >= Version("2.0.0")
