b23eb1fc18d51d012658fe10c7d80e618092f1f1
Leonardo Vida <lleonardovida@gmail.com>
2026-08-09 13:30:11 +0200

    chore: support DuckDB 1.5.5 (#148)

--- duckdb_sqlalchemy/__init__.py
+++ duckdb_sqlalchemy/__init__.py
@@ -137,7 +137,7 @@ else:
 try:
     __version__ = package_version("duckdb-sqlalchemy")
 except PackageNotFoundError:  # pragma: no cover - source tree import fallback
-    __version__ = "1.5.4.7"
+    __version__ = "1.5.5"
 sqlalchemy_version = sqlalchemy.__version__
 SQLALCHEMY_VERSION = Version(sqlalchemy_version)
 SQLALCHEMY_2 = SQLALCHEMY_VERSION >= Version("2.0.0")
